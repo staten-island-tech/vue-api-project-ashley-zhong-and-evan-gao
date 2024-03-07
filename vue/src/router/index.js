aeaedc9cab87ce9cd7e70f22dbe4288987294596
Evan Gao <gaoevan37@gmail.com>
2024-03-07 09:24:18 -0500

    asdfasdasdasd

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -6,16 +6,24 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/barchart',
+      name: 'Bar Chart',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/BarChartView.vue')
+    },
+    {
+      path: '/piechart',
+      name: 'Pie Chart',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/PieChartView.vue')
     }
   ]
 })
---
+++ vue/src/views/PieChartView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import chart from '@/components/chart.vue'
+const url = 'https://data.cityofnewyork.us/resource/jb7j-dtam.json'
+// chart js
+</script>
+
+<template>
+  <main>
+    <chart />
+  </main>
+</template>
