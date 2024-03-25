ea1fa48b4f6e2a767268e645048c6f22918f5a64
Evan Gao <gaoevan37@gmail.com>
2024-03-25 09:24:10 -0400

    a

--- vue/src/views/DeathData.vue
+++ vue/src/views/DeathData.vue
@@ -1,31 +1,29 @@
 <template>
-    <div>
-        <h1> {{ cause }}</h1>
-    </div>
+  <div>
+    <h1>test</h1>
+    <h1>{{ cause }}</h1>
+  </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-                cause: {
-
-                }
-            }
-        },
-        methods:{
-            getCause:{
-                async function(){
-                    let res= await fetch (`https://data.cityofnewyork.us/resource/jb7j-dtam.json?
-                    ${this.$route.params.id}`)
-                    let data = await res.json()
-                    this.cuase = data
-                }
-            }
-        }
+export default {
+  data() {
+    return {
+      cause: {}
     }
+  },
+  methods: {
+    getCause: {
+      async function() {
+        let res = await fetch(
+          `https://data.cityofnewyork.us/resource/jb7j-dtam.json?leading_cause=${this.$route.params.id}`
+        )
+        let data = await res.json()
+        this.cause = data
+      }
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
