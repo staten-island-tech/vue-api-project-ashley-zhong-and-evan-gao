8837f1cb927873dbb1ee7f15e0756b6e5f5414f6
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2024-03-25 11:30:16 -0400

    yayaya

--- vue/src/views/DeathData.vue
+++ vue/src/views/DeathData.vue
@@ -16,7 +16,7 @@
         methods:{
             getCause:
                 async function(){
-                    let res= await fetch (`https://data.cityofnewyork.us/resource/jb7j-dtam.json?leading_cause=${this.$route.params.id}`)
+                    let res= await fetch (`https://data.cityofnewyork.us/resource/jb7j-dtam.json?year=2014&leading_cause=${this.$route.params.id}`)
                     let data = await res.json()
                     console.log(data)
                     this.cause = data
