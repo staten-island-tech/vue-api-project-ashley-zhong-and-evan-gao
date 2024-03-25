bcd924bb99f6931e9198cbea80b437652f4dc738
Evan Gao <gaoevan37@gmail.com>
2024-03-25 11:37:00 -0400

    Update DeathData.vue

--- vue/src/views/DeathData.vue
+++ vue/src/views/DeathData.vue
@@ -1,31 +1,30 @@
 <template>
-    <div>
-        <h1> {{  }}</h1>
-    </div>
+  <div>
+    <h1>{{ cause }}</h1>
+  </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-                cause: {
-
-                }
-            }
-        },
-        methods:{
-            getCause:
-                async function(){
-                    let res= await fetch (`https://data.cityofnewyork.us/resource/jb7j-dtam.json?year=2014&leading_cause=${this.$route.params.id}`)
-                    let data = await res.json()
-                    console.log(data)
-                    this.cause = data
-                }
-        },
-        created() {
-            this.getCause(); // call the method when the component is created
-        }
+export default {
+  data() {
+    return {
+      cause: {}
     }
+  },
+  methods: {
+    getCause: async function () {
+      let res = await fetch(
+        `https://data.cityofnewyork.us/resource/jb7j-dtam.json?year=2014&leading_cause=${this.$route.params.id}`
+      )
+      let data = await res.json()
+      console.log(data)
+      this.cause = data
+    }
+  },
+  created() {
+    this.getCause() // call the method when the component is created
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
