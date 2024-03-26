6654f4bb9e418d82fcc974a1c7c24652e7eb4cdd
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2024-03-26 09:25:29 -0400

    hjkhjh

--- vue/src/components/PieChart.vue
+++ vue/src/components/PieChart.vue
@@ -1,20 +1,30 @@
 <template>
   <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+  <div> 
+    <h1>
+      hello
+      dfasdfadsf
+    </h1>
+  </div>
 </template>
 
 <script setup>
-import { Bar } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-} from 'chart.js'
+// import { Bar } from 'vue-chartjs'
+// import {
+//   Chart as ChartJS,
+//   Title,
+//   Tooltip,
+//   Legend,
+//   BarElement,
+//   CategoryScale,
+//   LinearScale
+// } from 'chart.js'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1{
+  font-size: large;
+}
+</style>
--- vue/src/views/DeathData.vue
+++ vue/src/views/DeathData.vue
@@ -1,34 +1,37 @@
 <template>
   <div>
-    <h1>{{ cause }}</h1>
+    <h1></h1>
   </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-                cause: {
+import PieChart from '@/components/PieChart.vue';
 
-                }
+export default {
+    
+    data() {
+        return {
+            cause: {
+
             }
-        },
-        mounted: async function () {
-            await this.getCause();
-        },
-        methods:{
-            getCause:
-                async function(){
-                    let res= await fetch (`https://data.cityofnewyork.us/resource/jb7j-dtam.json?year=2014&leading_cause=${this.$route.params.id}`)
-                    let data = await res.json()
-                    console.log(data)
-                    this.cause = data
-                }
-        },
-        // created() {
-        //     this.getCause(); 
-        // }
-    }
+        }
+    },
+    mounted: async function () {
+        await this.getCause();
+    },
+    methods: {
+        getCause:
+            async function () {
+                let res = await fetch(`https://data.cityofnewyork.us/resource/jb7j-dtam.json?year=2014&leading_cause=${this.$route.params.id}`)
+                let data = await res.json()
+                console.log(data)
+                this.cause = data
+            }
+    },
+    // created() {
+    //     this.getCause(); 
+    // }
+}
 
 </script>
 
