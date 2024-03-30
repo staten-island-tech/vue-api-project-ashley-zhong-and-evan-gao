30d69eec9a0a2973ac38090b9bb0ed1556a41719
Verviam <gaoevan37@gmail.com>
2024-03-30 13:32:52 -0400

    fixed search problems

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,22 +1,14 @@
 <template>
   <div class="home-page">
     <h3>Causes of Death in 2014</h3>
-    <input
-      type="text"
-      v-model="filteredDeathCauses"
-      placeholder="Search..."
-      @keyup.enter="findDeath"
+    <input type="text" v-model="searchedCause" placeholder="Search..." @keyup.enter="findDeath" />
+    <div>Finding Results for: {{ searchedCause }}</div>
+    <MainCard
+      v-for="cause in filteredUniqueLeadingCauses"
+      :key="cause"
+      :id="index"
+      :cause="cause"
     />
-    <button type="button" @click="findDeath" :disabled="!searchedCause">
-      Search Specific Causes
-    </button>
-    <div>Finding Results for: {{ filteredDeathCauses }}</div>
-    <MainCard v-for="cause in uniqueLeadingCauses" :key="cause" :id="index" :cause="cause" />
-
-    <p v-if="!deathData && !filteredDeathCauses">Loading...</p>
-    <pre v-else-if="filteredDeathCauses">{{ filteredDeathCauses }}</pre>
-
-    <!-- <MainCard v-else :key="componentKey" :deathData="getFilteredData"></MainCard> -->
   </div>
 </template>
 
@@ -25,10 +17,8 @@ import MainCard from '@/components/MainCard.vue'
 import { ref, onMounted, computed } from 'vue'
 
 const deathData = ref(null)
+const searchedCause = ref('')
 
-// const searchedCause = ref('')
-// const displayedCauseData = ref(null)
-// const componentKey = ref(0)
 // script setup makes composition api and script makes options https://www.youtube.com/watch?v=qRPSOXA1Fhw
 
 const API = 'https://data.cityofnewyork.us/resource/jb7j-dtam.json?year=2014'
@@ -47,35 +37,26 @@ async function fetchData(link) {
     console.log(error)
   }
 }
-// console.log(deathData)
 
 const uniqueLeadingCauses = computed(() => {
+  // takes all the items with same leading_causes and eliminates any extras
   if (deathData.value) {
     return [...new Set(deathData.value.map((item) => item.leading_cause))]
   }
   return ["didn't work properly"]
 })
-console.log(uniqueLeadingCauses.value)
+
+const filteredUniqueLeadingCauses = computed(() => {
+  if (searchedCause.value) {
+    return uniqueLeadingCauses.value.filter((cause) =>
+      cause.toLowerCase().includes(searchedCause.value.toLowerCase())
+    )
+  }
+  return uniqueLeadingCauses.value
+})
 
 onMounted(() => {
   fetchData(API)
-})
-
-function findDeath() {
-  if (searchedCause.value) {
-    const matchingData = deathData.value.filter((item) =>
-      item.leading_cause.toLowerCase().includes(searchedCause.value.toLowerCase())
-    )
-    displayedCauseData.value = matchingData
-    componentKey.value += 1
-  }
-}
-const getFilteredData = computed(() => {
-  if (uniqueLeadingCauses.value) {
-    return uniqueLeadingCauses.value
-  } else {
-    return []
-  }
 })
 </script>
 
