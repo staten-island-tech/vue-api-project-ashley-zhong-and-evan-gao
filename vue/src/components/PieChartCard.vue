9e6156d4e7aacdf80c557d736f9a2ea8d752a806
Verviam <gaoevan37@gmail.com>
2024-03-31 17:08:11 -0400

    IT DISPLAYSYSSSS

--- vue/src/components/PieChartCard.vue
+++ vue/src/components/PieChartCard.vue
@@ -1,54 +1,48 @@
 <template>
   <div class="PieChartContainer">
     <h1>Pie Chart</h1>
-
-    <Pie 
-    :options="chartOptions"
-    :data="chartData" />
+    <Pie :options="chartOptions" :data="chartData" />
   </div>
 </template>
 
 <script>
 import { Pie } from 'vue-chartjs'
-// import {
-//   Chart as ChartJS,
-//   Title,
-//   Tooltip,
-//   Legend,
-//   BarElement,
-//   CategoryScale,
-//   LinearScale
-// } from 'chart.js'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+
 ChartJS.register(ArcElement, Tooltip, Legend)
 
-// ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 export default {
   name: 'PieChartCard',
-  components: {Pie}, 
-  // extends: Pie,
-  data() {
-      return { 
-        chartData: {
-          labels: [ 'Men', 'Women'],
-          datasets: [ { 
-            backgroundColor: [ '#5a5e9a', '#777aaf'],
-            data: [ this.menProp, this.womenProp] } ]
-        },
-        chartOptions: {
-          responsive: true
-        }
-      }
-      },
-
+  components: { Pie },
   props: {
     menProp: {
       type: Number,
+      required: true
     },
-
     womenProp: {
       type: Number,
-    },
+      required: true
+    }
+  },
+  computed: {
+    chartData() {
+      return {
+        labels: ['Men', 'Women'],
+        datasets: [
+          {
+            backgroundColor: ['#5a5e9a', '#777aaf'],
+            data: [this.menProp, this.womenProp]
+          }
+        ]
+      }
+    }
+  },
+  data() {
+    return {
+      chartOptions: {
+        responsive: true
+      }
+    }
   }
 }
 </script>
