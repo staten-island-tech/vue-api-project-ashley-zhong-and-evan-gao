c6875e5627bf40cfd29dfdbe4af3a2e65871a045
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2024-03-30 21:12:35 -0400

    hellppp -- cannot populate men and women

--- vue/src/components/PieChart.vue
+++ vue/src/components/PieChart.vue
@@ -1,27 +1,39 @@
 <template>
-    <pie-chart :chart-data="chartData" />
+  <div>
+    <!-- <canvas :id="chartId" width="400" height="400"></canvas> -->
+  </div>
 </template>
 
 <script >
 import { Pie } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
   name: 'PieChart',
   extends: Pie,
   props: {
-    chartData: {
-      type: Object,
-      required: true,
-    },
+    menProp: Array,
+    womenProp: Array, 
+
+    // chartData: {
+    //   type: Array,
+    //   required: true
+    // },
+    // chartId: {
+    //   type: String,
+    //   default: 'pie-chart'
+    // }
   },
-  mounted() {
-    this.renderChart(this.chartData, {
-      responsive: true,
-      maintainAspectRatio: false,
-    });
-    console.log(chartData)
-  },
-};
+  // mounted() {
+  //   this.renderChart(this.chartData, {
+  //     responsive: true,
+  //     maintainAspectRatio: false
+  //   });
+  // }
+}
+
 </script>
 
 <style lang="scss" scoped>
--- vue/src/views/DeathData.vue
+++ vue/src/views/DeathData.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>    
-        <!-- <pie-chart v-if="chartData.datasets.length > 0" :chart-data="chartData" /> -->
+    <div>
+        <!-- <PieChart v-if="loaded"
+            :menProp="men"
+            :womenProp="women" /> -->
         <h1>hello</h1>
     </div>
 </template>
@@ -8,61 +10,105 @@
 
 <script>
 import PieChart from '@/components/PieChart.vue';
-// import { Bar } from 'vue-chartjs'
+import { Pie } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 
 
 
 export default {
     name: 'PieChart',
-  components: { PieChart },
-
+    components: { PieChart },
     data() {
         return {
-            cause: {},
+            loaded: false,
+            men:0,
+            women:0,
+
+
         }
     },
-    computed: {
-        chartData() {
-            const genderData = {
-        men: 0,
-        women: 0,
-      };
-      
-            this.cause.forEach((item) => {
-        if (item.sex === 'M') {
-            genderData.men += parseInt(item.deaths);
-        } else if (item.sex === 'F') {
-            genderData.women += parseInt(item.deaths);
-        }
-      });
-      return {
-        datasets: [
-        {
-          label: 'Gender',
-          data: [genderData.men, genderData.women],
-          backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)'],
-        },
-      ],
-      };
-
-        }
-
-    },
-    
-    mounted: async function () {
-
-        await this.getCause();
+    mounted: function(){
+        this.fetchData();
     },
     methods: {
-        getCause:
-            async function () {
-                let res = await fetch(`https://data.cityofnewyork.us/resource/jb7j-dtam.json?year=2014&leading_cause=${this.$route.params.id}`)
-                let data = await res.json()
-                console.log(data)
-                this.cause = data
+        fetchData: async function() {
+          this.loaded = false
+
+            try{
+                const res = await fetch(`https://data.cityofnewyork.us/resource/jb7j-dtam.json?year=2014&leading_cause=${this.$route.params.id}`);
+                const data = await res.json();
+
+                data.forEach((item) => {
+                   if (item.sex === 'M') {
+                       this.men += parseInt(item.deaths);
+                   } else if (item.sex === 'F') {
+                       this.women += parseInt(item.deaths);
+                   }
+               });
+
+                this.loaded=true
+            } catch(error){
+                console.log(error);
             }
-    },
+        },
+        
+  },
+
+    // computed: {
+    //     chartData() {
+    //         const genderData = {
+    //     men: 0,
+    //     women: 0,
+    //   };
+
+    //     this.cause.forEach((item) => {
+    //     if (item.sex === 'M') {
+    //         genderData.men += parseInt(item.deaths);
+    //     } else if (item.sex === 'F') {
+    //         genderData.women += parseInt(item.deaths);
+    //     }
+    //   });
+    //   return {
+    //     datasets: [
+    //     {
+    //       label: 'Gender',
+    //       data: [genderData.men, genderData.women],
+    //       backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)'],
+    //     },
+    //   ],
+    //   };
+
+    //     }
+
+    // },
+    //---------------------------help=-======
+    // mounted: async function () {
+
+    //     await this.getCause();
+    // },
+    // methods: {
+    //     getCause:
+    //         async function () {
+    //             let res = await fetch(`https://data.cityofnewyork.us/resource/jb7j-dtam.json?year=2014&leading_cause=${this.$route.params.id}`)
+    //             let data = await res.json()
+    //             console.log(data)
+    //             this.cause = data
+    //             data.forEach((item) => {
+    //                 if (item.sex === 'M') {
+    //                     this.men += parseInt(item.deaths);
+    //                 } else if (item.sex === 'F') {
+    //                     this.women += parseInt(item.deaths);
+    //                 }
+    //             });
+    //             this.loaded=true
+    //         }
+    // },
+
+
+
     // created() {
     //     this.getCause();
     // }
