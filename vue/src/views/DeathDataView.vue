c3c38ad615b5bdd52392a53b536770cb1d62470e
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2024-03-31 22:17:07 -0400

    OMG IT WORKS

--- vue/src/views/DeathDataView.vue
+++ vue/src/views/DeathDataView.vue
@@ -3,21 +3,35 @@
     <div class="men">Number of Men: {{ men }}</div>
     <div class="women">Number of Women: {{ women }}</div>
     <PieChartCard :menProp="men" :womenProp="women"></PieChartCard>
+    <BarChartCard 
+    :Black_NonHispanicProp="Black_NonHispanic"
+    :White_NonHispanicProp="White_NonHispanic"
+    :HispanicProp="Hispanic"
+    :Asian_PacificIslanderProp="Asian_PacificIslander"
+    :OtherProp="Other"
+    :UnknownProp="Unknown"></BarChartCard>
   </div>
 </template>
 
 <script>
 import PieChartCard from '@/components/PieChartCard.vue'
+import BarChartCard from '@/components/BarChartCard.vue'
 
 export default {
   name: 'AllCharts',
-  components: { PieChartCard },
+  components: { PieChartCard , BarChartCard},
 
   data() {
     return {
       loaded: false,
       men: 0,
-      women: 0
+      women: 0,
+      Black_NonHispanic: 0,
+      White_NonHispanic: 0,
+      Hispanic:0, 
+      Asian_PacificIslander: 0, 
+      Other:0, 
+      Unknown: 0, 
     }
   },
   mounted: function () {
@@ -47,6 +61,26 @@ export default {
             if (item.deaths != '.') {
               this.women += parseInt(item.deaths)
             }
+          }
+        })
+        this.data.forEach((item)=> {
+          if(item.race_ethnicity === "Black Non-Hispanic"){
+            this.Black_NonHispanic += parseInt(item.deaths)
+          }
+          if(item.race_ethnicity === "White Non-Hispanic"){
+            this.White_NonHispanic += parseInt(item.deaths)
+          }
+          if(item.race_ethnicity === "Hispanic"){
+            this.Hispanic += parseInt(item.deaths)
+          }
+          if(item.race_ethnicity === "Asian and Pacific Islander"){
+            this.Asian_PacificIslander += parseInt(item.deaths)
+          }
+          if(item.race_ethnicity === "Other Race/ Ethnicity"){
+            this.Other += parseInt(item.deaths)
+          }
+          if(item.race_ethnicity === "Not Stated/Unknown"){
+            this.Unknown += parseInt(item.deaths)
           }
         })
       } catch (error) {
