a29b622c5a5f9ffe370cd58ec3358af89b544867
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2024-04-01 00:02:53 -0400

    fdasfas

--- vue/src/views/BarChartView.vue
+++ vue/src/views/BarChartView.vue
@@ -1,48 +1,63 @@
 <template>
+  <div class="pie-chart">Pie Chart</div>
   <div>
-    <h1>Leading Cause of Death by Race</h1>
-    <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
   </div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-} from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
 export default {
-  name: 'BarChart',
-  components: { Bar },
+  name: 'AllPieChart',
+  components: { },
+
   data() {
     return {
-      chartData: {
-        labels: ['January', 'February', 'March'],
-        datasets: [{ data: [40, 20, 12] }]
-      },
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          x: {
-            grid: {
-              display: false
-            }
-          },
-          y: {
-            grid: {
-              display: false
-            }
-          }
+      AccidentsExceptDrugPosioning: 0, 
+      NephritisNephroticSyndromeNephrisis: 0, 
+      CerebrovascularDisease: 0, 
+      ChronicLowerRespiratory: 0, 
+      AllOther: 0,   
+      Alzheimer:0, 
+      MentalBehavioral: 0,
+      EssentialHypertension: 0,
+      Influenza: 0,
+      DiabetesMellitus: 0, 
+      CertainConditions: 0, 
+      MalignantNeoplasms: 0, 
+      SelfHarm: 0, 
+      HIV: 0, 
+      CongenitalMalformations: 0, 
+      Assault: 0, 
+      Septicemia: 0,
+      ChronicLiver: 0,  
+
+
+    }
+  },
+  mounted: function () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: async function () {
+      if (this.loaded) return
+      this.loaded = true
+      try {
+        this.data = null
+        const res = await fetch(
+          `https://data.cityofnewyork.us/resource/jb7j-dtam.json?year=2014`
+        )
+        if (res.status >= 200 && res.status < 300) {
+          this.data = await res.json()
+        } else {
+          throw new Error(res.statusText)
         }
+        console.log(this.data)
+        this.data.forEach((item)=> {
+          if(item.leading_cause === "Accidents Except Drug Posioning (V01-X39, X43, X45-X59, Y85-Y86)"){
+            this.AccidentsExceptDrugPosioning += parseInt(item.deaths)
+          }
+        })
+      } catch (error) {
+        console.log(error)
       }
     }
   }
@@ -50,7 +65,11 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  font-size: large;
-}
+/* @media (min-width: 1024px) {
+  .pie-chart {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
 </style>
--- vue/src/views/PieChartView.vue
+++ vue/src/views/PieChartView.vue
@@ -1,24 +1,17 @@
 <template>
-  <div class="pie-chart">Pie Chart</div>
-  <div>
-    <canvas id="pieChart"></canvas>
-  </div>
+
 </template>
 
 <script>
-// import PieChart from '@/components/PieChart.vue'
-// export default {
-//   name: 'App',
-//   components: { PieChart }
-// }
+
 </script>
 
 <style scoped>
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   .pie-chart {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
-}
+} */
 </style>
